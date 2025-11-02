990288c2ae1d5c42f5d7de3a0c062e21406171b1
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2025-11-02 17:18:13 +0100

    fix: Add margin-top to Spotify embed iframe for better spacing

--- src/views/mainView.vue
+++ src/views/mainView.vue
@@ -61,7 +61,7 @@ onMounted(() => {
         </div>
       </div>
       <iframe
-        style="border-radius: 12px"
+        style="border-radius: 12px ; margin-top: 20px;"
         src="https://open.spotify.com/embed/track/3eE2OyYKMf3YIFS1e74ulg?utm_source=generator"
         width="100%"
         height="352"
