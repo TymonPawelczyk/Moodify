102e2d097a5b83fa417d7f33d9d8eb32d26d84c8
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2025-11-02 17:17:44 +0100

    feat: Add Spotify embed iframe to main view

--- src/views/mainView.vue
+++ src/views/mainView.vue
@@ -60,6 +60,15 @@ onMounted(() => {
           Press Enter to send • Shift+Enter for newline
         </div>
       </div>
+      <iframe
+        style="border-radius: 12px"
+        src="https://open.spotify.com/embed/track/3eE2OyYKMf3YIFS1e74ulg?utm_source=generator"
+        width="100%"
+        height="352"
+        frameBorder="0"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
     </div>
   </div>
 </template>
